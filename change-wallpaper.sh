2219da0dd9e2c383b2f5bb5e7addcacca3e1ce9f
Harshavardhan M <harsha7vardhan@gmail.com>
2022-07-28 20:18:11 +0530

    works on pop

---
+++ change-wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+osname="$(hostname)"
+savedir="/tmp/wallpaper.png"
+
+# saves image in tmp directory
+curl https://i.imgur.com/S0kmuoAh.jpg > savedir
+
+# pop os
+if [ $osname == 'pop-os' ]
+then
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${savedir}"
+gsettings set org.gnome.desktop.background picture-uri "file://${savedir}"
+gsettings set org.gnome.desktop.background picture-options 'scaled'
+fi
